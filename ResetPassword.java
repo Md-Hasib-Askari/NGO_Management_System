7ccf6fcce05e0ab60e8a4ce08bf324768dc33b44
Robin Podder <97159516+RobinPodder@users.noreply.github.com>
2023-04-16 22:56:27 +0600

    Update ResetPassword.java

--- ResetPassword.java
+++ ResetPassword.java
@@ -14,14 +14,14 @@ import javax.swing.ImageIcon;
 
 public class ResetPassword extends JFrame{
     private String root = "C:\\Users\\Robin\\Desktop\\NIrmul Project\\mavenproject1\\src\\main\\java\\com\\mycompany\\mavenproject1";
-    private javax.swing.JLabel ConfirmPasswordLabel;
-    private javax.swing.JPanel ConfirmPasswordPanel;
-    private javax.swing.JPasswordField ConfirmPasswordPasswordField;
-    private javax.swing.JLabel NewPasswordLabel;
-    private javax.swing.JPasswordField NewPasswordPasswordField;
-    private javax.swing.JPanel ResetPasswordPanel;
-    private javax.swing.JButton SubmitBtn;
-    private javax.swing.JPanel newPasswordPanel;
+    private JLabel ConfirmPasswordLabel;
+    private JPanel ConfirmPasswordPanel;
+    private JPasswordField ConfirmPasswordPasswordField;
+    private JLabel NewPasswordLabel;
+    private JPasswordField NewPasswordPasswordField;
+    private JPanel ResetPasswordPanel;
+    private JButton SubmitBtn;
+    private JPanel newPasswordPanel;
     ResetPassword(){
         ResetPasswordPanel = new JPanel() {
             @Override
@@ -37,34 +37,34 @@ public class ResetPassword extends JFrame{
             }
         };
         ;
-        NewPasswordLabel = new javax.swing.JLabel();
-        NewPasswordPasswordField = new javax.swing.JPasswordField();
+        NewPasswordLabel = new JLabel();
+        NewPasswordPasswordField = new JPasswordField();
         ConfirmPasswordPanel =  new JPanel() {
             @Override
             public void paintComponent(Graphics g) {
                 g.drawImage(new ImageIcon(root+ "\\Static\\signinPanel.png").getImage(), 0,0,getWidth(),getHeight(),null);
             }
         };
-        ConfirmPasswordLabel = new javax.swing.JLabel();
-        ConfirmPasswordPasswordField = new javax.swing.JPasswordField();
-        SubmitBtn = new javax.swing.JButton();
+        ConfirmPasswordLabel = new JLabel();
+        ConfirmPasswordPasswordField = new JPasswordField();
+        SubmitBtn = new JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(null);
 
-        ResetPasswordPanel.setBackground(new java.awt.Color(0, 0, 0));
+        ResetPasswordPanel.setBackground(new Color(0, 0, 0));
         ResetPasswordPanel.setLayout(null);
 
         newPasswordPanel.setLayout(null);
 
-        NewPasswordLabel.setBackground(new java.awt.Color(61, 61, 61));
-        NewPasswordLabel.setFont(new java.awt.Font("Inter", 0, 12)); // NOI18N
+        NewPasswordLabel.setBackground(new Color(61, 61, 61));
+        NewPasswordLabel.setFont(new Font("Inter", 0, 12)); // NOI18N
         NewPasswordLabel.setText("NewPassword");
         newPasswordPanel.add(NewPasswordLabel);
         NewPasswordLabel.setBounds(20, 20, 170, 15);
 
-        NewPasswordPasswordField.setBackground(new java.awt.Color(234, 250, 241));
-        NewPasswordPasswordField.setFont(new java.awt.Font("Inter", 0, 14)); // NOI18N
+        NewPasswordPasswordField.setBackground(new Color(234, 250, 241));
+        NewPasswordPasswordField.setFont(new Font("Inter", 0, 14)); // NOI18N
         NewPasswordPasswordField.setText("jPasswordField1");
         NewPasswordPasswordField.setBorder(null);
         newPasswordPanel.add(NewPasswordPasswordField);
@@ -75,14 +75,14 @@ public class ResetPassword extends JFrame{
 
         ConfirmPasswordPanel.setLayout(null);
 
-        ConfirmPasswordLabel.setBackground(new java.awt.Color(61, 61, 61));
-        ConfirmPasswordLabel.setFont(new java.awt.Font("Inter", 0, 12)); // NOI18N
+        ConfirmPasswordLabel.setBackground(new Color(61, 61, 61));
+        ConfirmPasswordLabel.setFont(new Font("Inter", 0, 12)); // NOI18N
         ConfirmPasswordLabel.setText("ConfirmPassword");
         ConfirmPasswordPanel.add(ConfirmPasswordLabel);
         ConfirmPasswordLabel.setBounds(20, 20, 180, 15);
 
-        ConfirmPasswordPasswordField.setBackground(new java.awt.Color(234, 250, 241));
-        ConfirmPasswordPasswordField.setFont(new java.awt.Font("Inter", 0, 14)); // NOI18N
+        ConfirmPasswordPasswordField.setBackground(new Color(234, 250, 241));
+        ConfirmPasswordPasswordField.setFont(new Font("Inter", 0, 14)); // NOI18N
         ConfirmPasswordPasswordField.setText("jPasswordField2");
         ConfirmPasswordPasswordField.setBorder(null);
         ConfirmPasswordPanel.add(ConfirmPasswordPasswordField);
@@ -91,13 +91,13 @@ public class ResetPassword extends JFrame{
         ResetPasswordPanel.add(ConfirmPasswordPanel);
         ConfirmPasswordPanel.setBounds(75, 460, 480, 100);
 
-        SubmitBtn.setBackground(new java.awt.Color(46, 204, 113));
-        SubmitBtn.setFont(new java.awt.Font("Inter", 1, 18)); // NOI18N
-        SubmitBtn.setForeground(new java.awt.Color(255, 255, 255));
+        SubmitBtn.setBackground(new Color(46, 204, 113));
+        SubmitBtn.setFont(new Font("Inter", 1, 18)); // NOI18N
+        SubmitBtn.setForeground(new Color(255, 255, 255));
         SubmitBtn.setText("Submit");
         SubmitBtn.setBorder(null);
-        SubmitBtn.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
+        SubmitBtn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
                 //SubmitBtnActionPerformed(evt);
             }
         });
