979ca5778164d3e1449aab9c42662fdc6621fdea
Robin Podder <97159516+RobinPodder@users.noreply.github.com>
2023-04-23 17:44:41 +0600

    Add files via upload

---
+++ ForgetPassword.java
@@ -0,0 +1,166 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Graphics;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.Graphics;
+import javax.swing.ImageIcon;
+
+
+public class ForgetPassword extends JFrame{
+	private String root= "C:\\Users\\robin\\OneDrive\\Desktop\\Final Project\\";
+    private JPanel EmailPanel;
+    private JTextField EmailTextField;
+    private JLabel ForgetPassVerifyMsgLabel;
+    private JPanel ForgotPasswordPanel;
+    private JButton cancelBtn;
+    private JLabel emailLabel;
+    private JLabel emailLabel1;
+    private JButton sendBtn;
+	
+
+
+    ForgetPassword(){
+		
+		
+		
+		        emailLabel = new JLabel();
+        EmailPanel = new JPanel() {
+            @Override
+            public void paintComponent(Graphics g) {
+                g.drawImage(new ImageIcon(root+"Static\\signinPanel.png").getImage(), 0,0,getWidth(),getHeight(),null);
+                }
+            }
+            ;
+            EmailTextField = new JTextField();
+            emailLabel1 = new JLabel();
+            ForgotPasswordPanel =   new JPanel() {
+                @Override
+                public void paintComponent(Graphics g) {
+                    g.drawImage(new ImageIcon("C:\\Users\\Robin\\Desktop\\Java Project\\Static\\forgetPassword_bg.png").getImage(), 0,0,getWidth(),getHeight(),null);
+                    }
+                }
+                ;
+                ForgetPassVerifyMsgLabel = new JLabel();
+                cancelBtn = new JButton();
+                sendBtn = new JButton();
+
+                setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+                setBackground(new Color(204, 255, 204));
+                getContentPane().setLayout(null);
+
+                emailLabel.setFont(new Font("Inter", 0, 12)); 
+                emailLabel.setText("2023©NirmulFoundation");
+                getContentPane().add(emailLabel);
+                emailLabel.setBounds(534, 717, 140, 15);
+
+                EmailPanel.setBackground(new Color(255, 255, 255));
+                EmailPanel.setLayout(null);
+
+                EmailTextField.setBackground(new Color(234, 250, 241));
+                EmailTextField.setBorder(null);
+                EmailPanel.add(EmailTextField);
+                EmailTextField.setBounds(20, 40, 440, 40);
+
+                emailLabel1.setFont(new Font("Inter", 0, 12)); 
+                emailLabel1.setText("Email");
+                EmailPanel.add(emailLabel1);
+                emailLabel1.setBounds(20, 20, 30, 15);
+
+                getContentPane().add(EmailPanel);
+                EmailPanel.setBounds(75, 330, 480, 100);
+
+                ForgotPasswordPanel.setBackground(new Color(255, 255, 255));
+                ForgotPasswordPanel.setForeground(new Color(255, 255, 255));
+                ForgotPasswordPanel.setFont(new Font("Inter", 0, 14));
+                ForgotPasswordPanel.setRequestFocusEnabled(false);
+                ForgotPasswordPanel.setLayout(null);
+
+                ForgetPassVerifyMsgLabel.setFont(new Font("Inter", 0, 14));
+                ForgetPassVerifyMsgLabel.setText("We will send a verification code in your email account, enter the code and create a new password");
+                ForgotPasswordPanel.add(ForgetPassVerifyMsgLabel);
+                ForgetPassVerifyMsgLabel.setBounds(80, 270, 640, 18);
+/*
+                cancelBtn.setBackground(new Color(153, 255, 153));
+                cancelBtn.setFont(new Font("Inter", 0, 18)); 
+                cancelBtn.setForeground(new Color(255, 255, 255));
+                cancelBtn.setText("Cancel");
+                ForgotPasswordPanel.add(cancelBtn);
+                cancelBtn.setBounds(450, 460, 100, 40);
+				cancelBtn.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent evt) {
+						//cancelBtnActionPerformed(evt);
+            }
+        });
+*/
+                sendBtn.setBackground(new Color(46, 204, 113));
+                sendBtn.setFont(new Font("Inter", 0, 18)); 
+                sendBtn.setForeground(new Color(255, 255, 255));
+                sendBtn.setText("Send");
+				sendBtn.setBorder(null);
+                ForgotPasswordPanel.add(sendBtn);
+                sendBtn.setBounds(360, 575, 240, 60);
+				sendBtn.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent evt) {
+						sendBtnActionPerformed(evt);
+            }
+        }
+		);
+
+                getContentPane().add(ForgotPasswordPanel);
+                ForgotPasswordPanel.setBounds(0, 0, 1200, 750);
+
+                
+        //pack();
+		
+		setTitle("NGO Management System");
+		//setIconImage(new ImageIcon(root+"Stack\\titleIcon").getImage());
+        setVisible(true);
+        setResizable(false);
+        setSize(1200,750);
+		setLocationRelativeTo(null);
+
+        }
+		
+		
+private void sendBtnActionPerformed(java.awt.event.ActionEvent evt) {
+    // Get user input
+    String email = EmailTextField.getText();
+	
+	// Value from database
+	String EmailFromDB = "abcd@gmail.com";
+	
+    // Check if email is empty
+    if (email.isEmpty()) {
+        JOptionPane.showMessageDialog(this, "Please enter your email.");
+    } 
+    // Check if email matches the one in the database
+    else if (email.equals(EmailFromDB)) {
+        // Email is valid, dispose current GUI and open the VerifyAccount GUI
+        dispose();
+		
+        new ResetPassword();
+		
+		
+    } 
+    // Email is not valid
+    else {
+        JOptionPane.showMessageDialog(this, "Invalid email.");
+    }
+}
+
+
+
+        public static void main (String[] args){
+            System.setProperty("sun.java2d.uiScale", "1.0");
+            new ForgetPassword();
+
+        }
+
+
+    }
---
+++ ResetPassword.java
@@ -0,0 +1,172 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Graphics;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.Graphics;
+import javax.swing.ImageIcon;
+
+
+public class ResetPassword extends JFrame{
+    private String root = "C:\\Users\\Robin\\Desktop\\NIrmul Project\\mavenproject1\\src\\main\\java\\com\\mycompany\\mavenproject1";
+    private JLabel ConfirmPasswordLabel;
+    private JPanel ConfirmPasswordPanel;
+    private JPasswordField ConfirmPasswordPasswordField;
+    private JLabel NewPasswordLabel;
+    private JPasswordField NewPasswordPasswordField;
+    private JPanel ResetPasswordPanel;
+    private JButton SubmitBtn;
+    private JPanel newPasswordPanel;
+    ResetPassword(){
+        ResetPasswordPanel = new JPanel() {
+            @Override
+            public void paintComponent(Graphics g) {
+                g.drawImage(new ImageIcon(root+ "\\Static\\resetPassword_bg.png").getImage(), 0,0,getWidth(),getHeight(),null);
+            }
+        };
+        ;
+        newPasswordPanel = new JPanel() {
+            @Override
+            public void paintComponent(Graphics g) {
+                g.drawImage(new ImageIcon(root+ "\\Static\\signinPanel.png").getImage(), 0,0,getWidth(),getHeight(),null);
+            }
+        };
+        ;
+        NewPasswordLabel = new JLabel();
+        NewPasswordPasswordField = new JPasswordField();
+        ConfirmPasswordPanel =  new JPanel() {
+            @Override
+            public void paintComponent(Graphics g) {
+                g.drawImage(new ImageIcon(root+ "\\Static\\signinPanel.png").getImage(), 0,0,getWidth(),getHeight(),null);
+            }
+        };
+        ConfirmPasswordLabel = new JLabel();
+        ConfirmPasswordPasswordField = new JPasswordField();
+        SubmitBtn = new JButton();
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(null);
+
+        ResetPasswordPanel.setBackground(new Color(0, 0, 0));
+        ResetPasswordPanel.setLayout(null);
+
+        newPasswordPanel.setLayout(null);
+
+        NewPasswordLabel.setBackground(new Color(61, 61, 61));
+        NewPasswordLabel.setFont(new Font("Inter", 0, 12)); // NOI18N
+        NewPasswordLabel.setText("NewPassword");
+        newPasswordPanel.add(NewPasswordLabel);
+        NewPasswordLabel.setBounds(20, 20, 170, 15);
+
+        NewPasswordPasswordField.setBackground(new Color(234, 250, 241));
+        NewPasswordPasswordField.setFont(new Font("Inter", 0, 14)); // NOI18N
+        NewPasswordPasswordField.setText("jPasswordField1");
+        NewPasswordPasswordField.setBorder(null);
+        newPasswordPanel.add(NewPasswordPasswordField);
+        NewPasswordPasswordField.setBounds(20, 50, 440, 40);
+
+        ResetPasswordPanel.add(newPasswordPanel);
+        newPasswordPanel.setBounds(75, 340, 480, 100);
+
+        ConfirmPasswordPanel.setLayout(null);
+
+        ConfirmPasswordLabel.setBackground(new Color(61, 61, 61));
+        ConfirmPasswordLabel.setFont(new Font("Inter", 0, 12)); // NOI18N
+        ConfirmPasswordLabel.setText("ConfirmPassword");
+        ConfirmPasswordPanel.add(ConfirmPasswordLabel);
+        ConfirmPasswordLabel.setBounds(20, 20, 180, 15);
+
+        ConfirmPasswordPasswordField.setBackground(new Color(234, 250, 241));
+        ConfirmPasswordPasswordField.setFont(new Font("Inter", 0, 14)); // NOI18N
+        ConfirmPasswordPasswordField.setText("jPasswordField2");
+        ConfirmPasswordPasswordField.setBorder(null);
+        ConfirmPasswordPanel.add(ConfirmPasswordPasswordField);
+        ConfirmPasswordPasswordField.setBounds(20, 50, 440, 40);
+
+        ResetPasswordPanel.add(ConfirmPasswordPanel);
+        ConfirmPasswordPanel.setBounds(75, 460, 480, 100);
+
+        SubmitBtn.setBackground(new Color(46, 204, 113));
+        SubmitBtn.setFont(new Font("Inter", 1, 18)); // NOI18N
+        SubmitBtn.setForeground(new Color(255, 255, 255));
+        SubmitBtn.setText("Submit");
+        SubmitBtn.setBorder(null);
+        SubmitBtn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                SubmitBtnActionPerformed(evt);
+            }
+        });
+        ResetPasswordPanel.add(SubmitBtn);
+        SubmitBtn.setBounds(360, 575, 240, 60);
+
+        getContentPane().add(ResetPasswordPanel);
+        ResetPasswordPanel.setBounds(0, 0, 1200, 750);
+
+        pack();
+        
+		
+		setTitle("NGO Management System");
+		//setIconImage(new ImageIcon(root+"Static\\titleIcon").getImage());
+        setVisible(true);
+        setResizable(false);
+        setSize(1200,750);
+		setLocationRelativeTo(null);
+
+        }
+		
+		
+		
+		private void SubmitBtnActionPerformed(ActionEvent evt) {                                            
+		// Get input values from text fields
+			String newPassword = NewPasswordPasswordField.getText();
+			String confirmNewPassword = ConfirmPasswordPasswordField.getText();
+
+		// Constants from database
+			String emailFromDB = "abcd@gmail.com";
+			String oldPasswordFromDB = "12345678";
+			String newPasswordFromDB = "87654321";
+
+		// Check if any fields are empty
+			if (newPassword.isEmpty() || confirmNewPassword.isEmpty()) {
+			JOptionPane.showMessageDialog(this, "Please fill in all fields.");
+			} else {
+        // Check if new password and confirm new password match
+			if (newPassword.equals(confirmNewPassword)) {
+            // New password and confirm new password match, update password in database
+            oldPasswordFromDB = newPasswordFromDB;
+            JOptionPane.showMessageDialog(this, "Password updated successfully.");
+			
+			    // Close the current frame
+					dispose();
+    
+				// Create a new instance of the signupBtn class
+				//signupBtn signupBtn = new signupBtn();
+				// Display the sign-in form to the user
+					//signupBtn.setVisible(true);
+					new Signin();
+	
+			
+			
+			} else {
+            // New password and confirm new password do not match, display error message
+            JOptionPane.showMessageDialog(this, "New password and confirm new password do not match.");
+        }
+    }
+}
+
+		
+
+
+        public static void main (String[] args){
+            System.setProperty("sun.java2d.uiScale", "1.0");
+            new ResetPassword();
+
+        }
+
+
+    }
